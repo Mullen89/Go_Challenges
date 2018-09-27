2db659c9095c78ac7de639fa642fdaa3bade50f3
Eric Mullen <ericmullen1989@gmail.com>
2018-09-26 19:06:57 -0500

    Add files via upload

---
+++ Day29_BitwiseAND.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nk := strings.Split(readLine(reader), " ")
+
+		nTemp, err := strconv.ParseInt(nk[0], 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		kTemp, err := strconv.ParseInt(nk[1], 10, 64)
+		checkError(err)
+		k := int32(kTemp)
+
+		if ((k-1)|k) > n && k%2 == 0 {
+			fmt.Println(k - 2)
+		} else {
+			fmt.Println(k - 1)
+		}
+	}
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
