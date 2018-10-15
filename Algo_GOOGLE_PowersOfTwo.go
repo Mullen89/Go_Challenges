d6b98822219e5c3ea0fa4737d5996ff6dee54e75
Eric Mullen <ericmullen1989@gmail.com>
2018-10-14 21:59:54 -0500

    Add files via upload

---
+++ Algo_GOOGLE_PowersOfTwo.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var temp_x uint64
+	var x float64
+
+	scanner.Scan()
+	temp_x, _ = strconv.ParseUint(scanner.Text(), 10, 10)
+	x = float64(temp_x)
+
+	fmt.Printf("%t\n", PowersOfTwo(x))
+
+}
+
+func PowersOfTwo(num float64) bool {
+	fmt.Println(num)
+	if num/2 > 2 {
+		return PowersOfTwo(num / 2)
+	} else if math.Mod(num, 2) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
